ab941bdfe3049f5d18e1d7395723d1b654452036
mnbvcxzlkjhg09 <159231100+mnbvcxzlkjhg09@users.noreply.github.com>
2024-02-07 13:01:14 +0530

    Add files via upload

---
+++ database1/database.py
@@ -0,0 +1,46 @@
+import datetime
+import motor.motor_asyncio
+
+class Database:
+
+    def __init__(self, uri, Cluster0):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.techvj = self._client.Cluster0
+        self.col = self.techvj.USERS
+
+    def new_user(self, id):
+        return dict(id=id, thumbnail=None)
+
+
+    async def add_user(self, id):
+        user = self.new_user(id)
+        await self.col.insert_one(user)
+
+    async def is_user_exist(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return True if user else False
+
+    async def total_users_count(self):
+        count = await self.col.count_documents({})
+        return count
+
+    async def get_all_users(self):
+        all_users = self.col.find({})
+        return all_users
+
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'id': int(user_id)})
+
+    async def set_thumbnail(self, id, file_id):
+        await self.col.update_one({'id': id}, {'$set': {'file_id': file_id}})
+
+    async def get_thumbnail(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('thumbnail', None)
+
+    async def set_caption(self, id, caption):
+        await self.col.update_one({'id': int(id)}, {'$set': {'caption': caption}})
+
+    async def get_caption(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('caption', None)
